a3ec8571544b6647a4fb3bea5397d773e7218598
ckontos <har.kontos@hotmail.com>
2023-06-15 01:24:06 +0300

    v1.0

--- src/gr/aueb/cf/exceptions/NegativeAmountException.java
+++ src/gr/aueb/cf/exceptions/NegativeAmountException.java
@@ -4,7 +4,7 @@ public class NegativeAmountException extends Exception{
     private static final long serialVersionUID = 1L;
 
     public NegativeAmountException(double amount) {
-        super("Amount" + amount + " is negative");
+        super("Amount" + amount + " is negative.");
     }
 
 }
