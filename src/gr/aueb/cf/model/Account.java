35293f0ef87bd6d00aedc39f2170acb81780a4c8
ckontos <har.kontos@hotmail.com>
2023-06-14 05:41:10 +0300

    BankApp v0.3 (not complete)

--- src/gr/aueb/cf/model/Account.java
+++ src/gr/aueb/cf/model/Account.java
@@ -1,11 +1,15 @@
 package gr.aueb.cf.model;
 
+import gr.aueb.cf.exceptions.InsufficientBalanceException;
+import gr.aueb.cf.exceptions.NegativeAmountException;
+import gr.aueb.cf.exceptions.SsnNotValidException;
+
 public class Account extends IdentifiableEntity{
     private User holder;
     private String iban;
     private double balance;
 
-    public Account() {  
+    public Account() {
     }
 
     public Account(User holder, String iban, double balance) {
@@ -45,5 +49,48 @@ public class Account extends IdentifiableEntity{
                 ", iban='" + iban + '\'' +
                 ", balance=" + balance +
                 '}';
+    }
+
+    // Public API
+
+    public void deposit(double amount) throws NegativeAmountException {
+        try {
+            if (amount < 0) {
+                throw new NegativeAmountException(amount);
+            }
+
+            balance += amount;
+        } catch (NegativeAmountException e) {
+            System.err.println("Error: Negative amount");
+            throw e;
+        }
+    }
+
+    public void withdraw(double amount, String ssn)
+            throws InsufficientBalanceException, SsnNotValidException, NegativeAmountException {
+        try {
+            if (amount < 0) {
+                throw new NegativeAmountException(amount);
+            }
+            if (amount > balance) {
+                throw new InsufficientBalanceException(getBalance(), amount);
+            }
+            if (!isSsnValid(ssn)) {
+                throw new SsnNotValidException(ssn);
+            }
+
+            balance -= amount;
+        } catch (InsufficientBalanceException | SsnNotValidException | NegativeAmountException e) {
+            System.out.println("Error: withdrawal");
+            throw e;
+        }
+    }
+
+    protected boolean isSsnValid(String ssn) {
+        if (ssn == null || getHolder().getSsn() == null) {
+            return false;
+        }
+
+        return holder.getSsn().equals(ssn);
     }
 }
